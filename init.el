a0346049097e24b1386f0ee8d30689c423022121
qcfu <qcfu@bu.edu>
2022-12-08 18:59:07 -0500

    stuff

--- init.el
+++ init.el
@@ -223,7 +223,7 @@
 		 (window-height . 0.3))))
 
 ;; appearance
-(load-theme 'modus-operandi)
+(load-theme 'modus-vivendi)
 (set-face-attribute 'default nil :font "Fira Code-14")
 (set-face-attribute 'variable-pitch nil :font "Fira Sans-16")
 
