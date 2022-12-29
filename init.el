77c8dfad50c658bdfbcf4fd7d7450051f1ecfc0f
qcfu <qcfu@bu.edu>
2022-12-29 13:26:51 -0500

    stuff

--- init.el
+++ init.el
@@ -231,6 +231,7 @@
 ;; tools
 (use-package tab-bar
   :config
+  '(tab-bar ((t (:inherit mode-line))))
   (setq tab-bar-new-tab-choice "*scratch*"))
 
 (use-package popwin
@@ -274,6 +275,7 @@
 (use-package standard-themes
   :straight t
   :config
+  (setq standard-themes-fringes nil)
   (load-theme 'standard-light t))
 
 (use-package smartparens
