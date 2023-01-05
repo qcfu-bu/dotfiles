612ac0b6ea9f360c55d9c2630d6930a18b75d18c
qcfu <qcfu@bu.edu>
2023-01-05 00:25:52 -0500

    stuff

--- init.el
+++ init.el
@@ -161,7 +161,6 @@
 (use-package counsel
   :straight t
   :init
-  (setq ivy-use-virtual-buffers t)
   (setq enable-recursive-minibuffers t)
   (setq ivy-re-builders-alist '((t . orderless-ivy-re-builder)))
   (add-to-list 'ivy-highlight-functions-alist '(orderless-ivy-re-builder . orderless-ivy-highlight))
