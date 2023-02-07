0aa66407cdc0e719d8b1dd411714ac75bfe4590c
qcfu <qcfu@bu.edu>
2023-02-06 20:31:54 -0500

    stuff

--- init.el
+++ init.el
@@ -205,6 +205,7 @@
   :defer t
   :after yasnippet
   :config
+  (setq eldoc-echo-area-use-multiline-p nil)
   (add-to-list 'eglot-server-programs
 	       '((tex-mode context-mode texinfo-mode bibtex-mode) . ("texlab"))))
 
