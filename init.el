a6426a946ae6d50b7f87548a2e9795d5484388f7
qcfu <qcfu@bu.edu>
2023-05-15 00:15:25 -0400

    stuff

--- init.el
+++ init.el
@@ -343,7 +343,7 @@
   :config
   (setq modus-vivendi-palette-overrides '((bg-main bg-dim)))
   (setq modus-operandi-palette-overrides '((bg-dim bg-main)))
-  (load-theme 'modus-vivendi t))
+  (load-theme 'modus-operandi t))
 
 (use-package electric
   :config
