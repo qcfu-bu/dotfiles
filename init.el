60299625c306acc41490fc2a7a80154621fa2e6e
qcfu <qcfu@bu.edu>
2023-06-13 11:26:02 -0400

    stuff

--- init.el
+++ init.el
@@ -238,8 +238,8 @@
 (use-package git-gutter-fringe
   :straight t
   :config
-  (define-fringe-bitmap 'git-gutter-fr:added [224] nil 16 '(center repeated))
-  (define-fringe-bitmap 'git-gutter-fr:modified [224] nil 16 '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
   (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom)
   (setq git-gutter-fr:side 'right-fringe)
   (global-git-gutter-mode t))
@@ -373,15 +373,10 @@
 ;; Appearance
 ;;------------------------------------------------------------------------------
 
-(use-package modus-themes
+(use-package doom-themes
   :straight t
   :config
-  (setq modus-themes-common-palette-overrides
-        '((bg-line-number-active unspecifed)
-          (bg-line-number-inactive unspecified)))
-  (setq modus-vivendi-palette-overrides '((bg-main bg-dim)))
-  (setq modus-operandi-palette-overrides '((bg-dim bg-main)))
-  (load-theme 'modus-vivendi t))
+  (load-theme 'doom-gruvbox t))
 
 (use-package doom-modeline
   :straight t
