a95d9e4f613922ef9e73eecb046926fc30f4f1b9
qcfu <qcfu@bu.edu>
2023-06-13 11:20:16 -0400

    stuff

--- init.el
+++ init.el
@@ -238,8 +238,8 @@
 (use-package git-gutter-fringe
   :straight t
   :config
-  (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
-  (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:added [224] nil 16 '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:modified [224] nil 16 '(center repeated))
   (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom)
   (setq git-gutter-fr:side 'right-fringe)
   (global-git-gutter-mode t))
