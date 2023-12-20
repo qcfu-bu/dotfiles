fd535adefd4c0c4e51845955dc27c1587e34fbcf
qcfu <qcfu@bu.edu>
2023-12-20 15:39:39 -0500

    remove custom

--- init.el
+++ init.el
@@ -869,15 +869,3 @@
   :keymaps 'python-mode-map
   "e" 'run-python
   "b" 'python-shell-send-buffer)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(safe-local-variable-values '((TeX-master . main.tex))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
