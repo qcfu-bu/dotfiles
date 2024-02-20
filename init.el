597289251a1d29ddfb2b13decc051061f4e7ad88
qcfu <qcfu@bu.edu>
2024-02-20 09:50:33 -0500

    test

--- init.el
+++ init.el
@@ -414,7 +414,7 @@
   :config
   (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
   (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
-  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom)
+  ;; (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom)
   (global-git-gutter-mode t))
 
 (use-package gitignore-templates
