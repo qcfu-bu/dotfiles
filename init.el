ae017c62f3f3bfa304f9428294146c8db9c0d4aa
qcfu <qcfu@bu.edu>
2024-09-09 10:25:14 -0400

    Revert "replace eglot with lsp-mode"
    This reverts commit 3427d967328583f7c74be4a979ff298b8c9520b8.

--- init.el
+++ init.el
@@ -503,23 +503,36 @@
   (prog-mode . flycheck-mode)
   (flycheck-mode . mp-flycheck-prefer-eldoc))
 
-;;;; lsp
-(use-package lsp-mode
+;;;; eldoc
+(use-package eldoc
   :straight t
-  :commands (lsp)
-  :init
-  (defun lsp-format-on-save ()
-    (add-hook 'before-save-hook 'lsp-format-buffer))
-  :config
-  (setq lsp-completion-provider :none
-        lsp-eldoc-enable-hover nil
-        lsp-headerline-breadcrumb-enable nil
-        lsp-modeline-code-actions-enable nil))
+  :custom
+  (eldoc-idle-delay 0)
+  (eldoc-display-functions '(eldoc-display-in-buffer)))
 
-(use-package lsp-ui
+(use-package eldoc-box
   :straight t
+  :custom-face
+  (eldoc-box-border ((t (:background "black"))))
+  (eldoc-box-body ((t (:background unspecified :inherit tooltip)))))
+
+;;;; eglot
+(use-package eglot
+  :straight t
+  :commands (eglot-ensure)
+  :init
+  (defun eglot-format-on-save ()
+    (add-hook 'before-save-hook 'eglot-format-buffer))
   :config
-  (setq lsp-ui-doc-show-with-cursor nil))
+  (setq eglot-ignored-server-capabilities '(:inlayHintProvider))
+  (advice-add 'eglot-completion-at-point :around #'cape-wrap-buster)
+  (add-to-list 'eglot-server-programs '((tex-mode context-mode texinfo-mode bibtex-mode) . ("texlab")))
+  (add-to-list 'eglot-server-programs '((c-mode c-ts-mode c++-mode c++-ts-mode objc-mode) . ("clangd"))))
+
+(use-package flycheck-eglot
+  :straight t
+  :after (flycheck eglot)
+  :config (global-flycheck-eglot-mode 1))
 
 ;;;; dired
 (use-package dired
@@ -623,7 +636,7 @@
 (use-package auctex
   :straight t
   :hook
-  ((LaTeX-mode . lsp)
+  ((LaTeX-mode . eglot-ensure)
    (LaTeX-mode . visual-line-mode)
    (LaTeX-mode . flyspell-mode)
    (LaTeX-mode . rainbow-delimiters-mode))
@@ -650,8 +663,8 @@
 (use-package tuareg
   :straight t
   :hook
-  (tuareg-mode . lsp)
-  (tuareg-mode . lsp-format-on-save)
+  (tuareg-mode . eglot-ensure)
+  (tuareg-mode . eglot-format-on-save)
   (tuareg-mode . utop-minor-mode)
   (tuareg-mode . tuareg-compile-setup)
   (tuareg-menhir-mode . tuareg-compile-setup)
@@ -667,7 +680,7 @@
 (use-package reason-mode
   :straight t
   :hook
-  (reason-mode . lsp)
+  (reason-mode . eglot-ensure)
   (reason-mode . reason-utop-setup)
   (reason-mode . reason-format-on-save-mode)
   (reason-mode . reason-compile-setup)
@@ -728,7 +741,7 @@
 ;;;; haskell
 (use-package haskell-mode
   :straight t
-  :hook (haskell-mode . lsp))
+  :hook (haskell-mode . eglot-ensure))
 
 ;;;; agda
 (use-package agda2-mode
@@ -764,12 +777,12 @@
   (setq c-default-style "k&r")
   (setq-default c-basic-offset 4)
   :hook
-  (c-mode . lsp)
-  (c++-mode . lsp))
+  (c-mode . eglot-ensure)
+  (c++-mode . eglot-ensure))
 
 ;;;; python
 (use-package python
-  :hook (python-mode . lsp)
+  :hook (python-mode . eglot-ensure)
   :config
   (setq python-shell-interpreter "python3"))
 
@@ -823,6 +836,7 @@
 
 ;;;;; help
 (spc-leader-def
+  "hh" 'eldoc-box-help-at-point
   "hv" 'describe-variable
   "hf" 'describe-function
   "hF" 'describe-face
