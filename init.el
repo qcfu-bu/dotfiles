91c4d2d43a574621b1b4da153c09992aaab961d2
Qiancheng Fu <qcfu@bu.edu>
2025-03-15 19:51:34 -0400

    replace eglot with lsp, add support for lean4

--- init.el
+++ init.el
@@ -466,23 +466,17 @@
   (eldoc-box-body ((t (:background unspecified :inherit tooltip)))))
 
 ;;;; eglot
-(use-package eglot
+(use-package lsp-mode
   :straight t
-  :commands (eglot-ensure)
+  :commands (lsp)
   :init
-  (defun eglot-format-on-save ()
-    (add-hook 'before-save-hook 'eglot-format-buffer))
+  (defun lsp-format-on-save ()
+    (add-hook 'before-save-hook 'lsp-format-buffer))
   :config
-  (setq eglot-ignored-server-capabilities '(:inlayHintProvider))
-  (advice-add 'eglot-completion-at-point :around #'cape-wrap-buster)
-  (add-to-list 'eglot-server-programs '((tex-mode context-mode texinfo-mode bibtex-mode) . ("texlab")))
-  (add-to-list 'eglot-server-programs '((python-mode python-ts-mode) . ("pyright-langserver" "--stdio")))
-  (add-to-list 'eglot-server-programs '((c-mode c-ts-mode c++-mode c++-ts-mode objc-mode) . ("clangd")))
-  (add-to-list 'eglot-server-programs '((rust-mode rust-ts-mode) .
-                                        ("rust-analyzer" :initializationOptions
-                                         ( :procMacro (:enable t)
-                                           :cargo ( :buildScripts (:enable t)
-                                                    :features "all"))))))
+  (setq lsp-completion-provider :none
+        lsp-eldoc-enable-hover nil
+        lsp-headerline-breadcrumb-enable nil
+        lsp-modeline-code-actions-enable nil))
 
 ;;;; dired
 (use-package dired
@@ -590,7 +584,7 @@
 (use-package auctex
   :straight t
   :hook
-  ((LaTeX-mode . eglot-ensure)
+  ((LaTeX-mode . lsp)
    (LaTeX-mode . visual-line-mode)
    (LaTeX-mode . flyspell-mode)
    (LaTeX-mode . rainbow-delimiters-mode))
@@ -617,8 +611,8 @@
 (use-package tuareg
   :straight t
   :hook
-  (tuareg-mode . eglot-ensure)
-  (tuareg-mode . eglot-format-on-save)
+  (tuareg-mode . lsp)
+  (tuareg-mode . lsp-format-on-save)
   (tuareg-mode . utop-minor-mode)
   (tuareg-mode . tuareg-compile-setup)
   (tuareg-menhir-mode . tuareg-compile-setup)
@@ -634,7 +628,7 @@
 (use-package reason-mode
   :straight t
   :hook
-  (reason-mode . eglot-ensure)
+  (reason-mode . lsp)
   (reason-mode . reason-utop-setup)
   (reason-mode . reason-format-on-save-mode)
   (reason-mode . reason-compile-setup)
@@ -682,6 +676,16 @@
   (setq proof-splash-enable nil
         proof-three-window-mode-policy 'hybrid))
 
+;;;; lean4
+(use-package lean4-mode
+  :commands lean4-mode
+  :straight
+  (lean4-mode
+   :type git
+   :host github
+   :repo "leanprover-community/lean4-mode"
+   :files ("*.el" "data")))
+
 ;;;; why3
 (use-package why3
   :load-path "~/.opam/default/share/emacs/site-lisp"
@@ -695,7 +699,7 @@
 ;;;; haskell
 (use-package haskell-mode
   :straight t
-  :hook (haskell-mode . eglot-ensure))
+  :hook (haskell-mode . lsp))
 
 ;;;; agda
 (use-package agda2-mode
@@ -728,7 +732,7 @@
 ;;;; rust
 (use-package rust-mode
   :straight t
-  :hook (rust-mode . eglot-ensure)
+  :hook (rust-mode . lsp)
   :init
   (setq rust-format-on-save t))
 
@@ -738,12 +742,12 @@
   (setq c-default-style "k&r")
   (setq-default c-basic-offset 4)
   :hook
-  (c-mode . eglot-ensure)
-  (c++-mode . eglot-ensure))
+  (c-mode . lsp)
+  (c++-mode . lsp))
 
 ;;;; python
 (use-package python
-  :hook (python-mode . eglot-ensure)
+  :hook (python-mode . lsp)
   :config
   (setq python-shell-interpreter "python3"))
 
@@ -947,6 +951,14 @@
   "pp" 'proof-process-buffer
   "pr" 'proof-retract-buffer
   "pk" 'proof-shell-exit)
+
+;;;;; lean4
+(spc-local-leader-def
+  :keymaps 'lean4-mode
+  "b" 'lean4-lake-build
+  "t" 'lean4-toggle-info-buffer
+  "r" 'lean4-refresh-file-dependencies
+  "k" 'quail-show-key)
 
 ;;;;; ocaml
 (spc-local-leader-def
